1daf4730dd21f464151086df97521bf26fd3dc00
Hitesh-Bhadani <hiteshbhadani99@gmail.com>
2025-06-08 23:04:42 +0530

    Fix Vercel deployment: serverless refactor

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const serverless = require("serverless-http");
-const app = require("../server");
+const serverless = require('serverless-http');
+const app = require('../server'); // assuming server.js is one level up
 
 module.exports = serverless(app);
